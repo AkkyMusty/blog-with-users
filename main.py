c54628b2d6792f6cfcd96d58813d85dab3e7740b
AkkyMusty <153207112+AkkyMusty@users.noreply.github.com>
2024-01-13 20:19:29 +0000

    Admin only authentication for creating new post, editing and deleting old post added

--- main.py
+++ main.py
@@ -69,6 +69,16 @@ class User(db.Model, UserMixin):
 with app.app_context():
     db.create_all()
 
+
+
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if current_user.id != 1:
+            return abort(403)
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route('/register', methods=["POST", "GET"])
 def register():
     form = RegisterForm()
@@ -111,6 +121,7 @@ def login():
 
 
 @app.route('/logout')
+@admin_only
 def logout():
     logout_user()
     return redirect(url_for('get_all_posts'))
@@ -130,8 +141,9 @@ def show_post(post_id):
     return render_template("post.html", post=requested_post)
 
 
-# TODO: Use a decorator so only an admin user can create a new post
+
 @app.route("/new-post", methods=["GET", "POST"])
+@admin_only
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
@@ -149,8 +161,9 @@ def add_new_post():
     return render_template("make-post.html", form=form)
 
 
-# TODO: Use a decorator so only an admin user can edit a post
+
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+@admin_only
 def edit_post(post_id):
     post = db.get_or_404(BlogPost, post_id)
     edit_form = CreatePostForm(
@@ -171,8 +184,9 @@ def edit_post(post_id):
     return render_template("make-post.html", form=edit_form, is_edit=True)
 
 
-# TODO: Use a decorator so only an admin user can delete a post
+
 @app.route("/delete/<int:post_id>")
+@admin_only
 def delete_post(post_id):
     post_to_delete = db.get_or_404(BlogPost, post_id)
     db.session.delete(post_to_delete)
